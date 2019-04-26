dde67290e18321a02ffeefb95cdc48f2be19a0ec
Damy Bosch <info@damybosch.nl>
2019-04-26 18:51:10 +0200

    update eigen website

---
+++ website/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Damy Bosch</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+        <div class="site-branding">
+            <a>Damy Bosch</a>
+        </div>
+        <div class="site-navigation"></div>
+        </div>
+    </header>
+    <div class="teaser">
+        <div class="container">
+        <h1>Damy Bosch</h1>
+        <div>
+        <p>Front-end Developer</p>
+        <a href="#" class="btn">Contact</a>
+        <p>of</p>
+        <a href="#" class="not-btn">Curricullum</a>
+        </div>
+        </div>
+        <div class="square"></div>
+        <div class="square1"></div>
+        
+    </div>
+
+    <div class="skills">
+        <div class="blockInner">
+        <div class="container">
+        <h2>skills</h2>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
